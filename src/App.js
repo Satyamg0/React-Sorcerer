c6abc4e472d6630c861efab6eee13abc6f58fad1
Satyam Gupta <97743996+Satyamg0@users.noreply.github.com>
2024-02-20 12:54:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TextEditor from "./components/Editor/Draft";
+import "draft-js/dist/Draft.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <TextEditor />
+    </div>
+  );
+};
+
+export default App;
